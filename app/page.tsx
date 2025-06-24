6ffc7be57a2fba4b5c79f0a692a1fceae0a08f58
Muhammed Sanjid <sanjid357@gmail.com>
2025-06-24 14:20:00 +0530

    metadata

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,36 @@ import Navbar from "@/components/Navbar";
 import HorizontalScrollSection from "@/components/HorizontalScroll";
 import ContactFAB from "@/components/FAB";
 
+export const metadata = {
+  title: "GT3 Porsche Showcase",
+  description:
+    "Explore the legendary Porsche GT3RS | Interactive showcase with animations and design details.",
+  openGraph: {
+    title: "GT3 Porsche Showcase",
+    description:
+      "Explore the legendary Porsche GT3RS | Interactive showcase with animations and design details.",
+    url: "https://gt3.sanjid.shop",
+    siteName: "GT3 Porsche",
+    images: [
+      {
+        url: "https://gt3.sanjid.shop/og-cover.png",
+        width: 1200,
+        height: 630,
+        alt: "GT3 Porsche Cover Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "GT3 Porsche Showcase",
+    description:
+      "Explore the legendary Porsche GT3RS | Interactive showcase with animations and design details.",
+    images: ["https://gt3.sanjid.shop/og-cover.png"],
+  },
+};
+
 export default function Home() {
   return (
     <div className="h-full w-full">
